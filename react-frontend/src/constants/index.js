f37280f092410853ce80960f72f19050040b8fd3
Anil Dhakal <35804798+pyserve@users.noreply.github.com>
2024-10-01 08:15:50 -0400

    Update index.js

--- react-frontend/src/constants/index.js
+++ react-frontend/src/constants/index.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://metacode.pythonanywhere.com/";
-export const API_URL = "http://localhost:8000/";
+export const API_URL = "https://metacode.pythonanywhere.com/";
+// export const API_URL = "http://localhost:8000/";
